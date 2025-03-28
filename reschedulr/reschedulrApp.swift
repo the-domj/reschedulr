8f2ea85f63e5709387653c0fe8474e5807da73e8
the-domj <djt@nlmandc.com>
2025-03-27 21:36:22 -0700

    Initial commit

---
+++ reschedulr/reschedulrApp.swift
@@ -0,0 +1,17 @@
+//
+//  reschedulrApp.swift
+//  reschedulr
+//
+//  Created by Dominick Trupiano on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct reschedulrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ reschedulrTests/reschedulrTests.swift
@@ -0,0 +1,17 @@
+//
+//  reschedulrTests.swift
+//  reschedulrTests
+//
+//  Created by Dominick Trupiano on 3/27/25.
+//
+
+import Testing
+@testable import reschedulr
+
+struct reschedulrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
